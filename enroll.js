2287edb131f6e80a6cc07a31c90b7a395c0364ec
海芋 <35823337+Zuriches@users.noreply.github.com>
2018-01-26 14:00:03 +0800

    Delete enroll.js

--- enroll.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-var enroll = document.getElementById("enroll");
-enroll.onclick = function(e){
-	e.preventDefault();
-	enroll.innerHTML = "报名成功";
-	enroll.style.background = "#27cb8b";
-	enroll.style.borderColor = "#27cb8b";
-}
